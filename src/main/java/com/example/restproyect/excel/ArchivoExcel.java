cc5fea487ce9c7ead30f59102ec8d504fe567b27
Maxi <maxiroselli@gmail.com>
2019-12-05 23:29:48 -0300

    Generador de excel

---
+++ src/main/java/com/example/restproyect/excel/ArchivoExcel.java
@@ -0,0 +1,77 @@
+package com.example.restproyect.excel;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArchivoExcel {
+	private final static String NOMBRE_ARCHIVO = "reporte.xlsx";
+	private String nombre;
+	private String nombre_hoja;
+	private ArrayList<String> HEADER;
+	
+	private XSSFWorkbook workbook;
+	private XSSFSheet sheet;
+	private int rowNumero;
+	
+	//Constructor si no es un bean y son clases que se general
+	public ArchivoExcel(String nombre, String nombre_hoja, ArrayList<String> header) {
+		super();
+		this.nombre = "";
+		this.nombre_hoja = "";
+		this.HEADER = new ArrayList<String>();
+		this.workbook = new XSSFWorkbook();
+		this.sheet = workbook.createSheet(this.nombre_hoja);
+		this.rowNumero = 0;
+		this.agregarFila(this.HEADER);
+	}
+	
+	//Constructor si es bean
+	public ArchivoExcel() {
+		super();
+		this.nombre = "";
+		this.nombre_hoja = "";
+		this.HEADER = new ArrayList<String>();
+		this.workbook = new XSSFWorkbook();
+		this.rowNumero = 0;
+	}
+	
+	//Utilizar si se utiliza como una clase comun sin ser bean
+	public void iniciarHojaExcel(String nombre, String nombre_hoja, ArrayList<String> header) {
+		this.nombre = nombre;
+		this.nombre_hoja = nombre_hoja;
+		this.HEADER = header;
+		this.sheet = workbook.createSheet(this.nombre_hoja);
+		this.agregarFila(this.HEADER);
+	}
+
+	public void agregarFila(ArrayList<String> valores) {
+		Row row = sheet.createRow(rowNumero++);
+		for(int colNumero=0; colNumero < valores.size(); colNumero++) {
+			Cell cell = row.createCell(colNumero);
+			cell.setCellValue(valores.get(colNumero));
+		}
+	}
+	
+	public void generarArchivoExcel() {      
+        try {
+            FileOutputStream outputStream = new FileOutputStream(this.NOMBRE_ARCHIVO);
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+       
+	};
+}
